1d3a01627374ece824da542d7d03d7605b132e21
polomonk <275880829@qq.com>
2020-07-25 22:26:30 +0800

    开放了视频逐帧播放功能

--- screen_shot_tool.py
+++ screen_shot_tool.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import filedialog, ttk
 import os
+import cv2
+import threading
+import time
 from PIL import ImageGrab, Image, ImageTk
 import configparser
 # 去除屏幕分辨率的影响。遇坑立牌
@@ -8,7 +11,7 @@ import ctypes
 ctypes.windll.user32.SetProcessDPIAware()
 
 
-class Window(Tk):
+class Window(Tk, threading.Thread):
     def __init__(self):
         super().__init__()
         self.config_raw = configparser.RawConfigParser()     # 定义配置文件的类
@@ -16,22 +19,24 @@ class Window(Tk):
         # 如果配置文件中的路径存在，则读取同级的全部文件，文件类型筛选放在img_next()
         if os.path.exists(self.config_raw.get('setting', 'img2open')):
             self.img_father_dir = os.path.dirname(self.config_raw.get('setting', 'img2open'))
-            self.imglist = os.listdir(self.img_father_dir)
+            self.img_list = os.listdir(self.img_father_dir)
             try:        # 尝试文件按数字排序
-                self.imglist.sort(key=lambda x: int(x.split('.')[0]))
+                self.img_list.sort(key=lambda x: int(x.split('.')[0]))
             except:
                 pass
-            self.imglist = iter(self.imglist)     # 将文件列表转换成可迭代形式
+            self.img_list = iter(self.img_list)     # 将文件列表转换成可迭代形式
 
         self.title('要测试的图片或视频:')
         # self.wm_overrideredirect(True)
-        self.geometry('900x600+400+300')       # 默认窗口的大小和离屏幕左上角的距离
+        self.geometry('1000x800+400+300')       # 默认窗口的大小和离屏幕左上角的距离
         self.configure(background='gray')       # 窗口背景设置成灰色
         self.img_path = StringVar()     # 创建图像路径的绑定字符变量
         self.img_path.set(self.config_raw.get('setting', 'img2open'))        # 读取配置文件并赋值
         self.cnt = 0        # 对已保存的图片进行计数
         self.canvas = None      # 图片画板
         self.show_photo = None  # 要在画板上显示的图片
+        self.cap = None     # 视频
+        self.isPlaying = False      # 视频播放标志位
         Label(self, text='打开', fg='blue', bg='gray', font=('Fixdsys', 12))\
             .grid(row=0, column=0, ipady=10, sticky=E)      # 放置静态文字：打开
         Entry(self, textvariable=self.img_path, width=50)\
@@ -61,7 +66,7 @@ class Window(Tk):
 
         self.canvas_bgn_x = IntVar()        # 创建画板的坐标整型变量
         self.canvas_bgn_y = IntVar()
-        self.selecting = False      # 状态位，是否正在选择要截图的区域
+        self.isSelecting = False      # 状态位，是否正在选择要截图的区域
         self.lastDraw = None        # 保存上一次拖到鼠标留下的矩形框，以便之后清除
         Label(self, text='保存目录', bg='gray', fg='blue', font=('Fixdsys', 12))\
             .grid(row=1, column=0, sticky=E)        # 放置静态文本：保存目录
@@ -70,21 +75,38 @@ class Window(Tk):
 
         self.meg = StringVar()      # 创建提示信息的字符变量
         self.meg.set('快捷键还没弄好')
-        Label(self, text='提示', bg='gray', fg='blue', font=('Fixdsys', 12))\
+        Label(self, text='提示', bg='gray', fg='blue', font=("Fixdsys", 12))\
             .grid(row=2, column=0, ipady=10, sticky=SE)     # 放置静态文本：提示
-        Label(self, textvariable=self.meg, bg='gray', fg='yellow', font=('Fixdsys', 12))\
+        Label(self, textvariable=self.meg, bg='gray', fg='yellow', font=("Fixdsys", 12))\
             .grid(row=2, column=1, ipady=10, sticky=SE)     # 放置绑定提示变量的文本，提示修改时文本也会随之改变
+        self.play_btn = ttk.Button(self, text='播放/暂停', state=DISABLED, command=self.thread_start)  # 视频播放按钮，逐帧变化很慢时可快速跳过
+        self.play_btn.grid(row=1, column=5)
         Button(self, text='quit', command=self.quit)\
-            .grid(row=1, column=5)      # 推出程序按键
+            .grid(row=1, column=6)      # 推出程序按键
+        self.started = False
+        self.thread = threading     # 创建一个线程类
+        self.event_obj = self.thread.Event()        # 创建线程事件
+        self.independent_task = self.thread.Thread(target=self.btn_play, args=(self.event_obj,))     # 创建一个线程去跑视频
+        self.independent_task.setDaemon(True)       # 回收机制
+
+    def thread_start(self):     # 将会占满cpu的视频放到独立的线程里跑
+        self.isPlaying = bool(1 - self.isPlaying)       # 播放状态位变化
+        if self.thread.Event.isSet(self.event_obj):     # 正在播放视频则阻塞视频播放
+            self.thread.Event.wait(self.event_obj)      #
+        else:
+            self.thread.Event.set(self.event_obj)       # 否则，清除阻塞
+        # print(self.event_obj.isSet())
+        if not self.started:        # 任务可以执行
+            self.started = True
+            self.independent_task.start()
 
     def next_img(self):     # 切换到迭代图像路径列表的下一个图片路径
-        file = imglist.__next__()
+        file = self.img_list.__next__()
         while not file.endswith(('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff')):
-            file = imglist.__next__()
+            file = self.img_list.__next__()
         return file
 
     def btn_img_chg(self):      # 改变图片路径
-        global imglist      # 声明图像列表为全局变量
         img2open = filedialog.askopenfilename()     # 打开文件
         if img2open is '':      # 如果中途退出则会返回空字符
             return -1
@@ -101,12 +123,12 @@ class Window(Tk):
             self.meg.set('保存目录没有完成设置')
         # 重新得到同级的全部文件并将图像列表转换为可迭代对象
         self.img_father_dir = os.path.dirname(self.config_raw.get('setting', 'img2open'))
-        self.imglist = os.listdir(self.img_father_dir)
+        self.img_list = os.listdir(self.img_father_dir)
         try:
-            self.imglist.sort(key=lambda x: int(x.split('.')[0]))
+            self.img_list.sort(key=lambda x: int(x.split('.')[0]))
         except:
             pass
-        self.imglist = iter(self.imglist)
+        self.img_list = iter(self.img_list)
 
     def btn_show(self):     # 显示图像
         img2open = self.img_path.get()      # 要显示图像的路径
@@ -127,32 +149,45 @@ class Window(Tk):
             self.canvas.grid(row=3, column=1, columnspan=15)        # 给画板安排足够大的空间
 
         elif img2open.endswith(('avi', 'mp4', 'rmvb', 'flv')):
-            # config_raw.set('setting', 'img2open', img2open)
-            self.meg.set('暂不支持视频格式')
+            if not self.cap:
+                self.cap = cv2.VideoCapture(img2open)
+                self.play_btn["state"] = NORMAL
+            ret, img = self.cap.read()
+            if ret:
+                img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+                self.show_photo = ImageTk.PhotoImage(img)       # 转换成画板能使用的格式
+                self.canvas = Canvas(self, bg='gray', width=self.winfo_screenwidth()//2, height=self.winfo_screenheight()//2)       # 创建画板
+                self.canvas.create_image(0, 0, anchor=NW, image=self.show_photo)        # 画板左上角对齐格式显示图像
+                self.canvas.bind('<B1-Motion>', self.onLeftButtonMove)      # 画板绑定鼠标左键移动事件
+                self.canvas.bind('<ButtonRelease-1>', self.onLeftButtonUp)      # 绑定松开鼠标左键事件
+                self.canvas.bind('<Button-1>', self.onLeftButtonDown)       # 绑定按下鼠标左键事件
+                self.canvas.grid(row=3, column=1, columnspan=15)        # 给画板安排足够大的空间
+                # self.meg.set('暂不支持视频格式')
         else:
             self.meg.set('文件类型错误')
 
     def btn_next(self):     # 切换到下一张图并显示
-        old_path = self.img_path.get()
-        new_path = self.next_img()
-        try:      # 如果文件名是纯数字排序的
-            old_path_num = int(old_path.split('.')[0])
-            new_path_num = int(new_path.split('.')[0])
-            while old_path_num >= new_path_num:
-                new_path = self.next_img()
+        if not self.cap:
+            old_path = self.img_path.get()
+            new_path = self.next_img()
+            try:      # 如果文件名是纯数字排序的
+                old_path_num = int(old_path.split('.')[0])
                 new_path_num = int(new_path.split('.')[0])
-        except ValueError:     # 如果文件名不是数字排序的
-            while os.path.samefile(old_path, new_path):
+                while old_path_num >= new_path_num:
+                    new_path = self.next_img()
+                    new_path_num = int(new_path.split('.')[0])
+            except ValueError:     # 如果文件名不是数字排序的
+                while os.path.samefile(old_path, new_path):
+                    new_path = self.next_img()
                 new_path = self.next_img()
-            new_path = self.next_img()     
 
-        real_path = self.img_father_dir + os.sep + new_path     # 保存当前图像路径
-        img_file = open('config.cfg')
-        self.config_raw.set("setting", "img2open", real_path)
-        self.config_raw.write(img_file)
-        img_file.close()
+            real_path = self.img_father_dir + os.sep + new_path     # 保存当前图像路径
+            img_file = open('config.cfg')
+            self.config_raw.set("setting", "img2open", real_path)
+            self.config_raw.write(img_file)
+            img_file.close()
 
-        self.img_path.set(real_path)        # 更改图像路径变量
+            self.img_path.set(real_path)        # 更改图像路径变量
         self.btn_show()     # 显示更改后的图像
 
     def btn_dir_chg(self):      # 更改保存目录
@@ -173,15 +208,29 @@ class Window(Tk):
         self.config_raw.write(directory)
         directory.close()
 
+    def btn_play(self, event):
+        ret, img = self.cap.read()
+        while ret and self.isPlaying:
+            img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            self.show_photo = ImageTk.PhotoImage(img)  # 转换成画板能使用的格式
+            self.canvas.create_image(0, 0, anchor=NW, image=self.show_photo)  # 画板左上角对齐格式显示图像
+            self.canvas.update()
+            cv2.waitKey(20)
+            ret, img = self.cap.read()
+        if not ret:
+            self.meg.set('片子已放完')
+        else:
+            self.meg.set('vip体验已结束')        # 其实这个问题我也搞不定= =!
+
     def onLeftButtonDown(self, event):      # 按下鼠标左键事件
         self.canvas_bgn_x.set(event.x)
         self.canvas_bgn_y.set(event.y)
         # 开始截图
-        self.selecting = True
+        self.isSelecting = True
 
     def onLeftButtonMove(self, event):      # 鼠标移动事件
         # 鼠标左键移动，显示选取的区域
-        if not self.selecting:
+        if not self.isSelecting:
             return
         try:  # 删除刚画完的图形，要不然鼠标移动的时候是黑乎乎的一片矩形
             self.canvas.delete(self.lastDraw)
@@ -191,7 +240,7 @@ class Window(Tk):
 
     def onLeftButtonUp(self, event):        # 鼠标左键松开事件
         # 获取鼠标左键抬起的位置，保存区域截图
-        self.selecting = False
+        self.isSelecting = False
         try:
             self.canvas.delete(self.lastDraw)
         except:
